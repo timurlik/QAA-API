f9f0d09f566c47260c32c5477de97bcbdd444672
timurlik <timurbibnev@gmail.com>
2023-02-11 14:26:20 +0300

    Made main initial settings.

---
+++ src/test/java/AbstractTest.java
@@ -0,0 +1,29 @@
+import org.junit.jupiter.api.BeforeAll;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public abstract class AbstractTest {
+    static Properties properties = new Properties();
+    private static InputStream configFile;
+    private static String apiKey;
+    private static String baseUrl;
+
+    @BeforeAll
+    static void initTest() throws IOException {
+        configFile = new FileInputStream("src/main/resources/my.properties");
+        properties.load(configFile);
+
+        apiKey = properties.getProperty("apiKey");
+        baseUrl = properties.getProperty("baseUrl");
+
+    }
+    public static String getApiKey() {
+        return apiKey;
+    }
+    public static String getBaseUrl() {
+        return baseUrl;
+    }
+}
